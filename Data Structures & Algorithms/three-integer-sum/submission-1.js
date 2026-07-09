791e6e676e39946b31509aee2bdc3c3f1cc80593
Emanuel <13490052+emanuelfrua@users.noreply.github.com>
2026-07-09 10:45:07 -0300

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.js
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const triplets = [];
+        nums.sort((a, b) => a - b);
+
+        let leftP;
+        let rightP;
+
+        for (let i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) {
+                continue;
+            }
+            const numFix = nums[i];
+            leftP = i + 1;
+            rightP = nums.length - 1;
+
+            while (leftP < rightP) {
+                const numB = nums[leftP];
+                const numC = nums[rightP];
+
+                const sum = numFix + numB + numC;
+
+                if (sum === 0) {
+                    triplets.push([numFix, numB, numC]);
+                    leftP++;
+                    rightP--;
+
+                    while (leftP < rightP && nums[leftP] === nums[leftP - 1]) {
+                        leftP++;
+                    }
+                    while (leftP < rightP && nums[rightP] === nums[rightP + 1]) {
+                        rightP++;
+                    }
+                } else if (sum < 0) {
+                    leftP++;
+                } else {
+                    rightP--;
+                }
+            }
+        }
+        return triplets;
+    }
+}
